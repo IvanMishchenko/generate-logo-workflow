27e62c136e6f5430086f53628bbb74ba7b182d23
Ivan Mishchenko <imishchenko@sii.pl>
2023-03-26 19:38:58 +0200

    add workflow install dep 17

--- run-stable-diffusion.py
+++ run-stable-diffusion.py
@@ -1,11 +1,13 @@
 import torch
 from diffusers import StableDiffusionPipeline
 from gradient import ModelsClient
+from gradient import MachinesClient
 
 # model_id = "CompVis/stable-diffusion-v1-4"
 # device = "cuda"
 
-print('---->>', gradient.list())
+machine = MachinesClient('8acb785a15e4232e4c3ba395fcfd8d')
+print('machine---->>', machine.list())
 
 # pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 # pipe = pipe.to(device)
