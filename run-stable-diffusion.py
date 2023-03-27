62d98a744d0dbf8a6f4531c832dab8b40bb3a1ee
Ivan Mishchenko <imishchenko@sii.pl>
2023-03-27 14:10:46 +0200

    add workflow install dep 17

--- run-stable-diffusion.py
+++ run-stable-diffusion.py
@@ -6,8 +6,10 @@ from gradient import MachinesClient
 # model_id = "CompVis/stable-diffusion-v1-4"
 # device = "cuda"
 
+notebook = NotebooksClient('8acb785a15e4232e4c3ba395fcfd8d')
 machine = MachinesClient('8acb785a15e4232e4c3ba395fcfd8d')
 print('machine---->>', machine.list())
+print('notebook---->>', notebook.list())
 
 # pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 # pipe = pipe.to(device)
